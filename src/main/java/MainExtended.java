16795440981b54010c8b909bea4699aef18a86a8
Maria Chybiak <m.chybiak@gmail.com>
2022-06-26 15:35:53 +0200

    0.8 Zmienne - Zadanie: deklarowanie i wywoływanie zmiennych

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,11 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String nameOfBook = "Harry Potter";
+        int numberOfPages = 354;
+        double bookThickness = 10.23;
+        short numberOfWords = 32767;
+        char bookSymbol = 'H';
+        boolean availability = true;
+        System.out.println(nameOfBook + "\n" + numberOfWords + "\n" + bookThickness + "\n" + numberOfWords + "\n" + bookSymbol + "\n" + availability);
+    }
+}
