e0e157e9adb76a76c0630738abffc730d647760a
Maria Chybiak <m.chybiak@gmail.com>
2022-07-12 20:24:54 +0200

    Moduł 2.3 Ćwiczenia: instrukcje warunkowe

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,23 @@
 public class FirstClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook();
-        System.out.println(notebook);
+
+        Notebook notebook = new Notebook(200, 500, 2005);
+        System.out.println(notebook.weight + " " + notebook.price);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkProfitability();
+
+        Notebook heavyNotebook = new Notebook(400, 800, 2022);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkProfitability();
+
+        Notebook oldNotebook = new Notebook(1000, 2000, 2020);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkProfitability();
+
     }
 }
--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Notebook {
+public class Notebook {
+    int weight;
+    int price;
+    int year;
+    public Notebook(int weight, int price, int year){
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+    }
+    public void checkPrice() {
+        if (this.price < 600) {
+            System.out.println("This notebook is very cheap.");
+        } else if (this.price >= 600 && this.price <= 1000) {
+            System.out.println("The price is good.");
+        } else {
+            System.out.println("This notebook is expensive.");
+        }
+    }
+
+    public void checkWeight() {
+        if (this.weight < 300) {
+            System.out.println("This notebook is light.");
+        } else if (this.weight >= 300 && this.weight <= 600) {
+            System.out.println("The notebook is quite heavy.");
+        } else {
+            System.out.println("This notebook is very heavy.");
+        }
+    }
+
+    public void checkProfitability() {
+        if (this.price < 1500 && this.year > 2010) {
+            System.out.println("This purchase is profitable.");
+        } else if (this.price > 1100 && this.year < 2018) {
+            System.out.println("This purchase is not profitable.");
+        } else {
+            System.out.println("Find another notebook");
+        }
+    }
 }
+
