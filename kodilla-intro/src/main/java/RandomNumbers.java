86d790bc4f017749880dfc23ecaf6c447974af3b
Maria Chybiak <m.chybiak@gmail.com>
2022-07-14 16:23:36 +0200

    2.5. Ćwiczenia: pętle

---
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -0,0 +1,16 @@
+public class RandomNumbers {
+    public static void main(String[] args){
+
+        public int getCountOfRandomNumber(int max){
+            RandomNumbers random = new RandomNumbers();
+            int result = 0;
+            int sum = 5000;
+            while (sum < max) {
+                int temp = (30);
+                sum = sum+temp;
+                result++;
+            }
+            System.out.println(result);
+        }
+    }
+}
---
+++ kodilla-intro/src/main/java/UserWywołanie.java
@@ -0,0 +1,27 @@
+public class UserWywołanie {
+
+    public static void main(String[] args){
+
+        User Jan = new User("Jan", 25);
+        User Anna = new User("Anna", 19);
+        User Zofia = new User("Zofia", 38);
+        User Adam = new User("Adam", 10);
+        User Karolina = new User("Karolina", 42);
+
+        String[] usersNames = {Jan.getNames(), Anna.getNames(), Zofia.getNames(), Adam.getNames(), Karolina.getNames()};
+
+        double[] usersAge = {Jan.getAge(), Anna.getAge(), Zofia.getAge(), Adam.getAge(), Karolina.getAge()};
+        System.out.println(usersAge.length);
+
+        double sum = 0.0;
+        int usersAmount = usersAge.length;
+
+        for(int i=0; i < usersAge.length; i++){
+            sum += usersAge[i];
+        }
+
+        double averageAgeOfUsers = sum / usersAmount;
+        System.out.println(averageAgeOfUsers);
+
+    }
+}
