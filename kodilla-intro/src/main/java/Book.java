2a40d786b99e05a98062184ac3b9e9ce7068f77d
Maria Chybiak <m.chybiak@gmail.com>
2022-07-16 17:33:58 +0200

    2.7 Zadanie: zmienne i metody statyczne

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,17 @@
+public class Book {
+    private String author;
+    private String title;
+
+
+    public Book(String author, String title){
+        this.author = author;
+        this.title = title;
+    }
+
+    public String toString() {
+        return "Autor: " + author + ", title: " + title;
+    }
+    public static Book of(String author, String title) {
+        return new Book(author,title);
+    }
+}
---
+++ kodilla-intro/src/main/java/BookWywołanie.java
@@ -0,0 +1,8 @@
+public class BookWywołanie {
+    public static void main(String[] args){
+
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+
+        System.out.println(book);
+    }
+}
