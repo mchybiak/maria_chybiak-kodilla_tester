4e8ccdf81c420b81b3676eb2cb5c4e38c57a74fa
Maria Chybiak <m.chybiak@gmail.com>
2022-07-23 18:36:05 +0200

    Zadanie: bankomat

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -0,0 +1,14 @@
+// Bank powinien mieć metody zwracające:
+//
+//całkowity bilans ze wszystkich bankomatów,
+//liczbę transakcji związanych z wypłatą,
+//liczbę transakcji związaną z wpłatą pieniędzy,
+//średnią wartość wypłaty,
+//średnią wartość wpłaty.
+
+
+package com.kodilla.bank.homework;
+
+public class Bank {
+
+}
--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMashine.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMashine.java
@@ -1,3 +1,8 @@
+// Ma ona zawierać tablicę ze zrealizowanymi transakcjami
+// (liczbowa wartość dodatnia, jeśli użytkownik wpłacił pieniądze i ujemna, jeśli użytkownik wypłacił pieniądze).
+// Dodaj metody zwracające saldo bankomatu oraz liczbę wykonanych transakcji.
+
+
 package com.kodilla.bank.homework;
 
 public class CashMashine {
