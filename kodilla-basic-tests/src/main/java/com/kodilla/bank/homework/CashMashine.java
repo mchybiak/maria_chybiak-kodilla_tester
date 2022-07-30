3b5b35c11599b3718de3a858f057ddee8ef5aae5
Maria Chybiak <m.chybiak@gmail.com>
2022-07-30 21:14:49 +0200

    4.1 Interfejsy

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMashine.java
+++
@@ -1,9 +0,0 @@
-// Ma ona zawierać tablicę ze zrealizowanymi transakcjami
-// (liczbowa wartość dodatnia, jeśli użytkownik wpłacił pieniądze i ujemna, jeśli użytkownik wypłacił pieniądze).
-// Dodaj metody zwracające saldo bankomatu oraz liczbę wykonanych transakcji.
-
-
-package com.kodilla.bank.homework;
-
-public class CashMashine {
-}
