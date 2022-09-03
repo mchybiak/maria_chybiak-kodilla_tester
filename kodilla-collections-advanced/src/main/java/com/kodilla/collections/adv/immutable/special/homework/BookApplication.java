5f67900af0c99760af24834ec9e8d6908acd3aa5
Maria Chybiak <m.chybiak@gmail.com>
2022-09-03 15:53:52 +0200

    5.4 Zadanie: księgozbiór.

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -0,0 +1,24 @@
+package com.kodilla.collections.adv.immutable.special.homework;
+
+public class BookApplication {
+    public static void main(String[] args) {
+
+        Book book = BookManager.createBook("Pan Tadeusz", "Adam Mickiewicz");
+        Book book1 = BookManager.createBook("Pan Tadeusz", "Adam Mickiewicz");
+        Book book2 = BookManager.createBook("Pan Tadeusz", "Adam Mickiewicz");
+
+        String x = "This is string";
+        String y = "This is string";
+
+        System.out.println(book);
+        System.out.println(book1);
+        System.out.println(book2);
+
+        System.out.println(x == y);
+
+        System.out.println(book.hashCode() == book1.hashCode());
+        System.out.println(book1.hashCode() == book2.hashCode());
+        System.out.println(book.hashCode() == book2.hashCode());
+
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -0,0 +1,25 @@
+package com.kodilla.collections.adv.immutable.special.homework;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BookManager {
+
+    static List<Book> books = new ArrayList<>();
+    public static Book createBook(String title, String author){
+        Book book = new Book(title, author);
+        return new Book(title, author);
+    }
+
+public static void checkingIfExist(Book book){
+        if (books.contains(book)){
+            System.out.println("You can't add this book. This book already exists.");
+        } else {
+            books.add(book);
+            System.out.println("You can add this book. This book is new.");
+        }
+}
+
+}
