862453eb35574dfe880eed0ebdc9f69db1468769
Maria Chybiak <m.chybiak@gmail.com>
2022-09-04 12:10:03 +0200

    6.1 Stream - programowanie funkcyjne.

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/User.java
@@ -0,0 +1,46 @@
+package com.kodilla.stream;
+
+import java.util.Objects;
+
+public class User {
+    private String username;
+    private int age;
+    private int numberOfPost;
+    private String group;
+
+    public User(String username, int age, int numberOfPost, String group) {
+        this.username = username;
+        this.age = age;
+        this.numberOfPost = numberOfPost;
+        this.group = group;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getNumberOfPost() {
+        return numberOfPost;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return getAge() == user.getAge() && getNumberOfPost() == user.getNumberOfPost() && Objects.equals(getUsername(), user.getUsername()) && Objects.equals(getGroup(), user.getGroup());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getAge(), getNumberOfPost(), getGroup());
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsersManager {
+    public static void main(String[] args) {
+        List<String> chemistGroupUsernames = filterChemistGroupUsernames();
+        System.out.println(chemistGroupUsernames);
+    }
+
+    private static List<String> filterChemistGroupUsernames() {
+        List<String> usernames = UserRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getGroup().equals("Chemists"))
+                .map(UsersManager::getUserName)
+                .collect(Collectors.toList());
+
+        return usernames;
+    }
+
+    public static String getUserName(User user) {
+        return user.getUsername();
+    }
+}
