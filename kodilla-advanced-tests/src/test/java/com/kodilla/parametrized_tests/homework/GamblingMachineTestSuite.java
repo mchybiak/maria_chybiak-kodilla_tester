d92556815c4576fded54ebc79b51c523635ccd2c
Maria Chybiak <m.chybiak@gmail.com>
2022-09-25 18:30:09 +0200

    7.2 Zadanie: Maszyna do gier.

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -12,7 +12,6 @@ class GamblingMachineTestSuite {
     GamblingMachine gamblingMachine = new GamblingMachine();
 
     @ParameterizedTest
-    @CsvFileSource()
 
     public void throwExceptionIfNumberIsNotCorrect(String numbers){ //wyjątek gdy liczba jest nieprawidłowa
         String[] n = numbers.split(" ");
@@ -30,7 +29,6 @@ class GamblingMachineTestSuite {
     }
 
     @ParameterizedTest
-    @CsvFileSource()
 
     public void  noExceptionIfNumberIsCorrect(String numbers) {
         String[] n = numbers.split(" ");
@@ -39,11 +37,4 @@ class GamblingMachineTestSuite {
         for (String number : n) {
             numbersInSet.add(number);
         }
-
-        Set<Integer> testValues = new HashSet<>();
-        numbersInSet.stream()
-                .mapToInt(a -> Integer.parseInt(a))
-                .forEach(b -> testValues.add(b));
-        noExceptionIfNumberIsCorrect(() -> gamblingMachine.howManyWins(testValues));
-    }
-}
+    }}
