069149fa7d4c3cd03028813fb8c24dd49699b6bb
Maria Chybiak <m.chybiak@gmail.com>
2022-10-07 21:50:59 +0200

    7.6 Mockito - ćwiczenia.

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/Client.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/Client.java
@@ -2,4 +2,5 @@ package com.kodilla.mockito;
 
 public interface Client {
     void receive(Notification notification);
+
 }
--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/Notification.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/Notification.java
@@ -1,4 +1,5 @@
 package com.kodilla.mockito;
 
 public interface Notification {
+
 }
