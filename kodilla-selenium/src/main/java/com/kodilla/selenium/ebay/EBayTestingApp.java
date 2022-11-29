030837cd37bb732257aa923354cdeddbc93436e7
Maria Chybiak <m.chybiak@gmail.com>
2022-11-29 16:01:22 +0100

    13.3 Selenium Webdriver - pierwsze testy. Zadanie: testy wyszukiwarki.

--- kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -1,22 +1,20 @@
-package com.kodilla.selenium.google;
+package com.kodilla.selenium.ebay;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GoogleExample {
+public class EBayTestingApp {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "Documents/Development/Kodilla/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        driver.get("https://www.ebay.com/");
 
-        WebElement inputField = driver.findElement(By.id("L2AGLb"));
-        inputField.click();
+        WebElement inputField = driver.findElement(By.name("_nkw"));
+        inputField.sendKeys("laptop");
 
-        WebElement inputField2 = driver.findElement(By.name("q"));
-        inputField2.sendKeys("Kodilla");
-        inputField2.submit();
+        WebElement clickField = driver.findElement(By.id("gh-btn"));
+        clickField.click();
     }
 }
-
