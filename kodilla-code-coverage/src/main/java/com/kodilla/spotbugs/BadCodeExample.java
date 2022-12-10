3564afa65c681aed45c9dc03b69a0c76df4a2bd3
Maria Chybiak <m.chybiak@gmail.com>
2022-12-10 13:08:07 +0100

    15.2 Zadanie: praca z Jacoco i SonarLint.

--- kodilla-code-coverage/src/main/java/com/kodilla/spotbugs/BadCodeExample.java
+++ kodilla-code-coverage/src/main/java/com/kodilla/spotbugs/BadCodeExample.java
@@ -1,10 +1,16 @@
 package com.kodilla.spotbugs;
 
+import java.util.Objects;
+
 public class BadCodeExample {
-    private String type;
+    private final String type;
+
+    public BadCodeExample(String type) {
+        this.type = type;
+    }
 
     public static double getPi() {
-        return 3.14;
+        return Math.PI;
     }
 
     public String getType() {
@@ -15,9 +21,12 @@ public class BadCodeExample {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        BadCodeExample that = (BadCodeExample) o;
+        return getType().equals(that.getType());
+    }
 
-        BadCodeExample badCodeExample = (BadCodeExample) o;
-
-        return type != null ? type.equals(badCodeExample.type) : badCodeExample.type == null;
+    @Override
+    public int hashCode() {
+        return Objects.hash(getType());
     }
 }
--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -1,10 +1,11 @@
 package com.kodilla.jacoco;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class YearTest {
-
     @Test
     public void shouldBeLeapYearIfDivisibleBy400() {
 
@@ -16,5 +17,33 @@ public class YearTest {
 
         //then
         assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy4() {
+        Year year = new Year(2020);
+        boolean isLeapYear = year.isLeap();
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleBy100() {
+        Year year = new Year(1900);
+        boolean isLeapYear = year.isLeap();
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleBy4And100() {
+        Year year = new Year(2001);
+        boolean isLeapYear = year.isLeap();
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleByOtherNumber() {
+        Year year = new Year(1999);
+        boolean isLeapYear = year.isLeap();
+        assertFalse(isLeapYear);
     }
 }
