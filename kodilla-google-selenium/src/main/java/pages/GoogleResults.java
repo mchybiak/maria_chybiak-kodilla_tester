d4e2f47f8b3f93201ec2797d146e5f3782a73245
Maria Chybiak <m.chybiak@gmail.com>
2022-12-12 00:42:55 +0100

    13.6 Page Object Model.

--- kodilla-google-selenium/src/main/java/pages/GoogleResults.java
+++ kodilla-google-selenium/src/main/java/pages/GoogleResults.java
@@ -1,4 +1,27 @@
 package pages;
 
-public class GoogleResults {
+import java.util.List;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class GoogleResults extends AbstractPage {
+
+    @FindBy(css = "div[class='g']")                  // [1]
+    private List<WebElement> results;                // [2]
+
+    public GoogleResults(WebDriver driver) {         // [3]
+        super(driver);                                // [4]
+        PageFactory.initElements(this.driver, this);  // [5]
+    }
+
+    public GoogleResults() {
+        super();
+    }
+
+    public void iSeeResults() {
+        System.out.println("I see results");
+        System.out.println(results.size());
+    }
 }
